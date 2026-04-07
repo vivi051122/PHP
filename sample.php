4783b3cf2153c773a0bb3ea2ae89023231d44ad8
vivi051122 <svt17.bsuga@gmail.com>
2026-04-07 18:08:41 +0900

    繰り返し処理について
    四則演算
    for文・foreach文
    if文
    break・continue文

--- sample.php
+++ sample.php
@@ -29,3 +29,29 @@ $ren = [
 echo"私の名前は{$nizi[0][0]}です。年齢は{$nizi[0][1]}歳{$nizi[0][2]}です。"."\n";
 echo $ren['name'];
 ?>
+
+<?php
+$i = 0;
+echo $i;
+echo $i++;
+echo $i;
+echo ++$i;
+?>
+
+<?php
+for($i =0;$i<4;$i++){
+    echo 3;
+}
+?>
+
+<?php
+for($i =0; $i<15; $i++){
+    if($i % 2 ===0){
+        continue;
+    }
+    if($i >13){
+        break;
+    }
+    echo $i."\n";
+}
+?>
