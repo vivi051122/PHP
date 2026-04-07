6ca39832bb04500eb5c5b6249b3bf15c159fb2cf
vivi051122 <svt17.bsuga@gmail.com>
2026-04-07 14:23:54 +0900

    配列から値の出力

--- sample.php
+++ sample.php
@@ -7,3 +7,25 @@ echo $sample;
 $age = 20;
 $test ="テスト"
 ?>
+
+<?php
+$array = [1,2,3];
+echo $array[0];
+echo $array[2];
+?>
+
+<?php
+$nizi = [
+    ['松下',29,'男性'],
+    ['高田',25,'男性'],
+    ['岡田',25,'女性']
+];
+
+$ren = [
+    'name'=>'田中',
+    'age'=>24,
+    'codelang'=>'HTML'
+];
+echo"私の名前は{$nizi[0][0]}です。年齢は{$nizi[0][1]}歳{$nizi[0][2]}です。"."\n";
+echo $ren['name'];
+?>
